7c007ef576a8c9b062226754cab8ff0407202ff2
Massimiliano Schindele <mschindele@unibz.it>
2019-06-03 21:25:19 +0200

    Add D and E Buildings

--- src/components/BuildingPlants.vue
+++ src/components/BuildingPlants.vue
@@ -7,6 +7,14 @@
       <CTwoPlant v-if="selected === 'C2'" />
       <CThreePlant v-if="selected === 'C3'" />
       <CFourPlant v-if="selected === 'C4'" />
+      <DZeroPlant v-if="selected === 'D0'" />
+      <DOnePlant v-if="selected === 'D1'" />
+      <EZeroPlant v-if="selected === 'E0'" />
+      <EOnePlant v-if="selected === 'E1'" />
+      <ETwoPlant v-if="selected === 'E2'" />
+      <EThreePlant v-if="selected === 'E3'" />
+      <EFourPlant v-if="selected === 'E4'" />
+      <EFivePlant v-if="selected === 'E5'" />
     </v-layout>
   </v-container>
 </template>
@@ -18,6 +26,14 @@ import COnePlant from "./svgplants/COnePlant";
 import CTwoPlant from "./svgplants/CTwoPlant";
 import CThreePlant from "./svgplants/CThreePlant";
 import CFourPlant from "./svgplants/CFourPlant";
+import DZeroPlant from "./svgplants/DZeroPlant";
+import DOnePlant from "./svgplants/DOnePlant";
+import EZeroPlant from "./svgplants/EZeroPlant";
+import EOnePlant from "./svgplants/EOnePlant";
+import ETwoPlant from "./svgplants/ETwoPlant";
+import EThreePlant from "./svgplants/EThreePlant";
+import EFourPlant from "./svgplants/EFourPlant";
+import EFivePlant from "./svgplants/EFivePlant";
 
 import { mapGetters } from "vuex";
 
@@ -29,7 +45,15 @@ export default {
     COnePlant,
     CTwoPlant,
     CThreePlant,
-    CFourPlant
+    CFourPlant,
+    DZeroPlant,
+    DOnePlant,
+    EZeroPlant,
+    EOnePlant,
+    ETwoPlant,
+    EThreePlant,
+    EFourPlant,
+    EFivePlant
   },
   computed: {
     ...mapGetters(["selected"])
