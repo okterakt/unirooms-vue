baa312ce3aebc93c2e7e3562ee68d746af0493fa
Massimiliano Schindele <mschindele@unibz.it>
2019-06-03 16:16:55 +0200

    Add date to Timetable

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 export default {
   async loadAllLectures() {
-    const response = await axios.get("/api");
+    var d = new Date();
+    var starttime = +d.setUTCHours(0, 0, 0, 0) / 1000;
+    var endtime = +d.setUTCHours(24, 0, 0, 0) / 1000;
+    const response = await axios.get(
+      "/api?starttime=" + starttime + "&endtime=" + endtime
+    );
     return response.data.data;
   }
 };
